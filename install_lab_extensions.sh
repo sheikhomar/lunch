f43c3a1e09a3fd0ed6cb991d7faefdfe1b9425a0
Omar Ali Sheikh <sheikhomar@users.noreply.github.com>
2020-02-26 07:09:03 +0100

    Script to install JupyterLab extensions.

---
+++ install_lab_extensions.sh
@@ -0,0 +1,11 @@
+jupyter labextension install @jupyter-widgets/jupyterlab-manager --no-build
+jupyter labextension install jupyterlab-plotly --no-build
+jupyter labextension install plotlywidget --no-build
+jupyter labextension install @krassowski/jupyterlab_go_to_definition --no-build
+jupyter labextension install @ijmbarr/jupyterlab_spellchecker --no-build
+jupyter labextension install @jupyterlab/toc --no-build
+jupyter labextension install @aquirdturtle/collapsible_headings --no-build
+
+# Build extensions
+jupyter lab build
+
